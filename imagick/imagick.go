675d39aa73bb89d1436b4cd95ef16f4a8f954838
xq262144 <xq262144@gmail.com>
2016-03-23 20:54:17 +0800

    Strip import path limitations and add additonal dependency library for static library linking

--- imagick/imagick.go
+++ imagick/imagick.go
@@ -2,9 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package imagick // import "gopkg.in/gographics/imagick.v2/imagick"
+package imagick
 
 /*
+#cgo LDFLAGS: -static -ljpeg -lpng -lwebp -lz -lgomp -lm -lbz2
 #cgo !no_pkgconfig pkg-config: MagickWand MagickCore
 */
 import "C"
